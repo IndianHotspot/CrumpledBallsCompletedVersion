e1bf1e8d75016790e0970514d84191c6fe66d403
IndianHotspot <70408224+IndianHotspot@users.noreply.github.com>
2020-11-18 18:31:28 -0800

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,37 @@
+class Paper{
+    constructor(x,y,radius) {
+        var options = {
+            isStatic:false,
+            'restitution':0.3,
+            'friction':1.0,
+            'density':2.0,
+            'frictionAir': 0.03,
+         
+        }
+
+        
+        this.x=x
+        this.y=y
+        this.radius=radius;
+        
+        this.body = Bodies.circle(this.x,this.y,this.radius, options);
+        World.add(world, this.body);
+
+        this.image = loadImage();
+      }
+      
+       
+      display(){
+        var pos =this.body.position;
+        //var angle=this.body.angle
+        push();
+        translate(pos.x,pos.y)
+        imageMode(CENTER)
+        fill("brown");
+        ellipseMode(RADIUS);
+        ellipse(0,0, this.radius,this.radius);
+        pop();
+      }
+
+
+    };
